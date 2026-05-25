840ee68e0d430f20e1ce7cf52a281d469ac7d5cf
milan_gataric_naovis <milan.gataric@naovis.com>
2026-05-25 17:55:13 +0200

    add poup changes

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,17 @@ createRoot(document.getElementById('root')).render(
         <BrowserRouter>
             <AuthProvider>
                 <App />
-                <ToastContainer position="top-right" autoClose={3000} />
+                <ToastContainer
+                    position="top-center"
+                    autoClose={4000}
+                    hideProgressBar={false}
+                    newestOnTop
+                    closeOnClick
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
             </AuthProvider>
         </BrowserRouter>
     </StrictMode>,

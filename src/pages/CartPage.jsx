27fad439cdbb68e70e16cdbc52fbd4f8a8de0cf0
milan <milan.gataric93@gmail.com>
2026-08-28 17:27:09 +0200

    add test optimistic UI for cart actions on the mobile

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -446,7 +446,7 @@ const CartPage = () => {
                         {t('cart.continueShopping')}
                     </button>
                 </div>
-                {/*{pending && <UndoBar message={pending.message} onUndo={undo} />}*/}
+                {pending && <UndoBar message={pending.message} onUndo={undo} />}
                 <UndoBar message="TEST" onUndo={() => {}} />
             </>
         );
